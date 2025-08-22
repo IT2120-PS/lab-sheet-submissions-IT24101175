7decd3052050de0f303a6c9dc98e006df9322df4
IT24101175 <IT24101175@my.sliit.lk>
2025-08-22 14:04:25 +0530

    ps lab 04 R

---
+++ Lab Sheet 04/ps_lab4_R.R
@@ -0,0 +1,29 @@
+setwd("C:\\Users\\IT24101175\\Desktop\\IT24101175_ps_lab_04")
+
+branch_data <- read.table("Exercise.txt", header = TRUE, sep = ",")
+
+boxplot(branch_data$Sales_X1,
+        main = "Boxplot for Sales",
+        ylab = "Sales",
+        col = "lightblue",
+        horizontal = FALSE)
+
+cat("Five-Number Summary for Advertising:\n")
+print(summary(branch_data$Advertising_X2))
+
+cat("IQR for Advertising:\n")
+print(IQR(branch_data$Advertising_X2))
+
+find_outliers <- function(x) {
+  Q1 <- quantile(x, 0.25)
+  Q3 <- quantile(x, 0.75)
+  IQR_val <- Q3 - Q1
+  lower_bound <- Q1 - 1.5 * IQR_val
+  upper_bound <- Q3 + 1.5 * IQR_val
+  x[x < lower_bound | x > upper_bound]
+}
+
+outliers_years <- find_outliers(branch_data$Years_X3)
+cat("Outliers in Years (Years_X3):\n")
+print(outliers_years)
+
